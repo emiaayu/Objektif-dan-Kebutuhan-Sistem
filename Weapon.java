e77352ddd559bf33fa6f4317066c9081c7b8cd48
emiaayu <emiaayucgirsang@gmail.com>
2026-03-15 07:53:51 +0700

    Create Weapon.java

---
+++ Weapon.java
@@ -0,0 +1,9 @@
+public class Weapon {
+    string name;
+    int damage;
+
+    void showWeapon() {
+        system.out.println("Nama Senjata : " + name);
+        system.out.println("Damage       : " + damage);
+    }
+}

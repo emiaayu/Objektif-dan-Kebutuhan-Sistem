b1573c3e12eccf8e4ba0eacb0a86477046926921
emiaayu <emiaayucgirsang@gmail.com>
2026-03-15 07:53:20 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) 
+    {
+        Weapon sabitKematian = new Weapon();
+        sabitKematian.name = "Sabit Kematian";
+        sabitKematian.damage = 170;
+
+        sabitKematian.showWeapon();
+    }
+}
+    

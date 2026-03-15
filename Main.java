97bc4bc7e8a354c04a221a7783c0b4a26bea63ee
emiaayu <emiaayucgirsang@gmail.com>
2026-03-15 08:10:03 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Weapon sabitKematian = new Weapon();
-        sabitKematian.name = "Excalibur";
-        sabitKematian.damage = 999;
+        Weapon Excalibur = new Weapon();
+        Excalibur.name = "Excalibur";
+        Excalibur.damage = 999;
 
-        sabitKematian.showWeapon();
+        Excalibur.showWeapon();
     }
 }
